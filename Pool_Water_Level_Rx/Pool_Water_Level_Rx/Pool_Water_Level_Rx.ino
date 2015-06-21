94c9653621d9e0be9dc1f676c4c3c6113f1aab12
Scott Goldthwaite <scott@goldthwaite.com>
2015-06-21 09:21:47 -0400

    v1.13 panstamp v2 API

--- Pool_Water_Level_Rx/Pool_Water_Level_Rx/Pool_Water_Level_Rx.ino
+++ Pool_Water_Level_Rx/Pool_Water_Level_Rx/Pool_Water_Level_Rx.ino
@@ -3,6 +3,10 @@ Pool water level sensor - Rx
 Use PanStamp to recieves data from water level sensor
 Acts as I2C slave and sends data to main Arduino pool controller
 
+
+To do:
+Find out if interrupts should be disabled in radioSignalInterrupt() so the wire wireRequestEvent interrupt doesn't interfere with panstamp interrupt
+ 
  
 PanStamp packet structure
 byte 0:     Rx ID - ID of Rx panStamp
@@ -36,14 +40,17 @@ Change log
 08/17/14 v1.10 - Added checksum. Formatting. Change real time level so 0 = level okay
 08/18/14 v1.11 - changed panStampOk from 0 to 1.
 08/18/14 v1.12 - Changed I2C_PACKET_LEN from 16 to 15, renamed some constants
+06/15/15 v1.13 - Changed panstamp timeout (PSTIMEOUT) from 2 minutes to 10 minutes.  Added VERSION constant and prints it in setup()
+                 Updated sketch to be compatible with panStamp V2 API
+*/
 
- */
-
+#define VERSION "v1.13"
 // #define PRINT_DEBUG // comment out to turn off serial printing
 
+#include <HardwareSerial.h>
 #include "EEPROM.h"       // panStamp address is saved to EEPROM http://www.arduino.cc/en/Reference/EEPROM
-#include "cc1101.h"       // http://code.google.com/p/panstamp/source/browse/trunk/arduino/libraries/panstamp/cc1101.h
-#include "panstamp.h"     // http://code.google.com/p/panstamp/source/browse/trunk/arduino/libraries/panstamp/panstamp.h
+//#include "cc1101.h"       // http://code.google.com/p/panstamp/source/browse/trunk/arduino/libraries/panstamp/cc1101.h
+//#include "panstamp.h"     // http://code.google.com/p/panstamp/source/browse/trunk/arduino/libraries/panstamp/panstamp.h
 #include <Wire.h>         // Used for I2C
 
 // This gets rid of compiler warning:  Only initialized variables can be placed into program memory area
@@ -51,36 +58,54 @@ Change log
 #define PROGMEM __attribute__(( section(".progmem.data") ))
 
 // The networkAdress of panStamp sender and receiver must be the same
-byte psNetworkAdress =          91;  // Network address for all pool panStamps
-byte psReceiverAddress =         5;  // Device address of this panStamp
-const byte ADDR_SLAVE_I2C =     21;  // I2C Slave address of this device
-const byte PANSTAMP_OFFLINE =  255;  // Send this to I2C master in the panStamp Rx address to indicate panStamp is offline
-const byte PANSTAMP_OK =         1;  // panStamp is successfully transmitting data
-const byte I2C_PACKET_LEN =     15;  // bytes in I2C Packet
-byte I2C_Packet[I2C_PACKET_LEN];    // Array to hold data sent over I2C to main Arduino
-
-// panStamp data http://code.google.com/p/panstamp/source/browse/trunk/arduino/libraries/panstamp/ccpacket.h
-CCPACKET packet;
-
-// The connection to the hardware chip CC1101 the RF Chip
-CC1101 cc1101;  // http://code.google.com/p/panstamp/wiki/CC1101class
-
+const byte g_RF_Channel =        0;   // panStamp channel
+byte g_psNetworkAddress[] = {91, 0};  // panStamp network address, aka SyncWord
+byte g_psReceiverAddress =       5;   // Device address of this panStamp
+const byte ADDR_SLAVE_I2C =     21;   // I2C Slave address of this device
+const byte PANSTAMP_OFFLINE =  255;   // Send this to I2C master in the panStamp Rx address to indicate panStamp is offline
+const byte PANSTAMP_OK =         1;   // panStamp is successfully transmitting data
+const byte I2C_PACKET_LEN =     15;   // bytes in I2C Packet
+      byte I2C_Packet[I2C_PACKET_LEN];      // Array to hold data sent over I2C to main Arduino
+const uint32_t PSTIMEOUT = 600000;  // Timeout for panStamps.  If no connections in 2 minutes, tell master that panStamp is offline
+uint32_t g_psTxTimer =  PSTIMEOUT;  // Initialize Tx timeout timer
 
 // flag indicates wireless packet has been received
-volatile boolean psPacketAvailable = false;        
+volatile boolean g_psPacketAvail = false;        
 
 // Function Prototypes
-void cc1101signalsInterrupt(void);
+void radioSignalInterrupt(CCPACKET *psPacket);
 void wireRequestEvent();
 
 //========================================================================================================================================
 // Handle interrupt from CC1101 (INT 0)
 //========================================================================================================================================
-void cc1101signalsInterrupt(void)
+void radioSignalInterrupt(CCPACKET *psPacket)
 {
-  // set the flag that a package is available
-  psPacketAvailable = true;
-} // end cc1101signalsInterrupt()
+  if( psPacket->crc_ok && psPacket->length > 1 )
+  { 
+    g_psPacketAvail = true;  // set the flag that a package is available
+    g_psTxTimer = millis() + PSTIMEOUT;  // reset timeout timer
+    // Copy data from panStamp packet to I2C packet array
+    I2C_Packet[0] =  ADDR_SLAVE_I2C;
+    I2C_Packet[1] =  PANSTAMP_OK;
+    I2C_Packet[2] =  psPacket->data[3];    // Low water level, Water level sensor 0 = level ok, 1 = level is low, 2 = sensor offline
+    I2C_Packet[3] =  psPacket->data[4];    // Real time Low water level: true = low water, false = water ok
+    I2C_Packet[4] =  psPacket->data[5];    // Is Lid Flat: true/false
+    I2C_Packet[5] =  psPacket->data[6];    // Accelerometer x-axis
+    I2C_Packet[6] =  psPacket->data[7];    // Accelerometer x-axis
+    I2C_Packet[7] =  psPacket->data[8];    // Accelerometer y-axis
+    I2C_Packet[8] =  psPacket->data[9];    // Accelerometer y-axis
+    I2C_Packet[9] =  psPacket->data[10];   // Accelerometer x-axis
+    I2C_Packet[10] = psPacket->data[11];  // Accelerometer x-axis
+    I2C_Packet[11] = psPacket->data[12];  // Battery voltage
+    I2C_Packet[12] = psPacket->data[13];  // Battery voltage
+    I2C_Packet[13] = psPacket->data[14];  // Reserved for leak detected in electronics
+    // checksum
+    I2C_Packet[I2C_PACKET_LEN - 1] = 0;
+    for ( byte cs = 0; cs < I2C_PACKET_LEN - 1; cs++ )
+    { I2C_Packet[I2C_PACKET_LEN - 1] += I2C_Packet[cs]; }
+  } 
+} // end radioSignalInterrupt()
 
 
 //========================================================================================================================================
@@ -91,17 +116,17 @@ void setup()
 
   Wire.begin(ADDR_SLAVE_I2C);    // Initiate the Wire library and join the I2C bus
   Wire.onRequest(wireRequestEvent); // Register a function to be called when a master requests data from this slave device. 
- 
-  cc1101.init(); // initialize the RF Chip in panStamp
-  cc1101.setSyncWord(&psNetworkAdress, false);  // Set network address (pointer), false parameter tells function not to save to EEPROM
-  // This receiverAddress needs to match the receiverAddress in the Tx panStamp
-  cc1101.setDevAddress(psReceiverAddress, false);  // false parameter tells function not to save to EEPROM
-  cc1101.enableAddressCheck(); // you can skip this line, because the default is to have the address check enabled
-  cc1101.setRxState(); // Set this panStamp to be a receiver
-  attachInterrupt(0, cc1101signalsInterrupt, FALLING); // Enable wireless reception interrupt
-  
-  #ifdef PRINT_DEBUG 
-  // Print device setup info
+
+  // Setup the panStamp radio
+  panstamp.radio.setChannel(g_RF_Channel);
+  panstamp.radio.setSyncWord(g_psNetworkAddress);  // Set network address, pointer to address
+  panstamp.radio.setDevAddress(g_psReceiverAddress);
+  panstamp.setPacketRxCallback(radioSignalInterrupt);  // Declare RF callback function
+
+  g_psTxTimer = millis() + PSTIMEOUT;  // Initialize Tx timeout timer
+
+   #ifdef PRINT_DEBUG 
+    // Print device setup info
     Serial.print(F("Radio Frequency = "));
     if(cc1101.carrierFreq == CFREQ_868)
     {Serial.println(F("868 Mhz"));}
@@ -113,6 +138,8 @@ void setup()
     Serial.println(cc1101.syncWord[0]);
     Serial.print(F("Device address =  "));
     Serial.println(cc1101.devAddress);
+    Serial.print("Sketch Version ");
+    Serial.println(VERSION);
   #endif
   
 
@@ -122,77 +149,16 @@ void setup()
 //========================================================================================================================================
 void loop()
 {
-  const uint32_t PSTIMEOUT = 120000;  // 2 minute timeout for panStamps.  If no connections in 2 minutes, tell master that panStamp is offline
-  static uint32_t psTxTimer = millis() + PSTIMEOUT;  // Initialize Tx timeout timer
   
-  // Get data from water level sensor panStamp
-  if(psPacketAvailable)
+  if(g_psPacketAvail)
   {
-    // clear the flag
-    psPacketAvailable = false;
-    
-    // Disable wireless reception interrupt so this code finishes executing without inturruption
-    detachInterrupt(0);
-    
-    if(cc1101.receiveData(&packet) > 0)
-    {
-      if (packet.crc_ok && packet.length > 1)
-      {  
-        psTxTimer = millis() + PSTIMEOUT;  // reset timeout timer
-        
-      #ifdef PRINT_DEBUG
-        // print received data
-        char buf[40];
-        sprintf(buf, "Received %d bytes from panStamp %d", packet.length, packet.data[1]);
-        Serial.println(buf);
-        Serial.print(F("Rx ID: "));
-        Serial.print(packet.data[0]);
-        Serial.print(F("   Tx ID: "));
-        Serial.println(packet.data[1]);
-        Serial.print(F("Low Water Status: "));
-        Serial.print(packet.data[3]);
-        Serial.print(F("   Low Water Real Time: "));
-        Serial.println(packet.data[4]);
-        if (packet.data[5] == true)
-        { Serial.println(F("Lid is level")); }
-        else
-        { Serial.println(F("Lid is not level")); }
-        Serial.print(F("Battery LSB: "));
-        Serial.print(packet.data[11]);
-        Serial.print(F("   Battery MSB: "));
-        Serial.println(packet.data[12]);
-        Serial.println();
-      #endif
-       
-        // Copy data from panStamp packet to I2C packet array
-        I2C_Packet[0] = ADDR_SLAVE_I2C;
-        I2C_Packet[1] = PANSTAMP_OK;
-        I2C_Packet[2] = packet.data[3];    // Low water level, Water level sensor 0 = level ok, 1 = level is low, 2 = sensor offline
-        I2C_Packet[3] = packet.data[4];    // Real time Low water level: true = low water, false = water ok
-        I2C_Packet[4] = packet.data[5];    // Is Lid Flat: true/false
-        I2C_Packet[5] = packet.data[6];    // Accelerometer x-axis
-        I2C_Packet[6] = packet.data[7];    // Accelerometer x-axis
-        I2C_Packet[7] = packet.data[8];    // Accelerometer y-axis
-        I2C_Packet[8] = packet.data[9];    // Accelerometer y-axis
-        I2C_Packet[9] = packet.data[10];   // Accelerometer x-axis
-        I2C_Packet[10] = packet.data[11];  // Accelerometer x-axis
-        I2C_Packet[11] = packet.data[12];  // Battery voltage
-        I2C_Packet[12] = packet.data[13];  // Battery voltage
-        I2C_Packet[13] = packet.data[14];  // Reserved for leak detected in electronics
-        // checksum
-        I2C_Packet[I2C_PACKET_LEN - 1] = 0;
-        for ( byte cs = 0; cs < I2C_PACKET_LEN - 1; cs++ )
-        { I2C_Packet[I2C_PACKET_LEN - 1] += I2C_Packet[cs]; }
-                                 
-      } // packet is okay
-    }  // got packet
-    
-    // Enable panStamp wireless reception interrupt
-    attachInterrupt(0, cc1101signalsInterrupt, FALLING);
-  }
+    panstamp.rxOff();  // turn panStamp off while processing data
+    g_psPacketAvail = false; // Reset panStamp paccket avail flag
+    panstamp.rxOn(); // Finished prcessing, turn panStamp back on 
+  }  
 
-  // If we haven't received data from transmitter in 2 minutes, Tx is considered offline
-  if ((long)(millis() - psTxTimer) > 0 )    
+  // If we haven't received data from transmitter in 10 minutes, Tx is considered offline
+  if ((long)(millis() - g_psTxTimer) > 0 )    
   { 
     // panStamp is offline, set I2C packet appropriately
     memset(I2C_Packet, 0, sizeof(I2C_Packet));  // clear array
@@ -202,9 +168,7 @@ void loop()
     // checksum
     for (byte cs = 0; cs < 3; cs++)
     { I2C_Packet[I2C_PACKET_LEN - 1] += I2C_Packet[cs]; }
-                                 
-  }  // end psPacketAvailable
-  
+  }  
   
 } // end loop()
 
